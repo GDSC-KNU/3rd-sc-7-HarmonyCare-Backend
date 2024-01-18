d072d6bfbc13d3d00173fec7bb45e819310341d7
lsmin3388 <lsmin3388_@naver.com>
2024-01-18 17:40:43 +0900

    feat: SpringBoot 3.1.7 개발환경 세팅

---
+++ src/main/java/com/harmonycare/HarmonycareApplication.java
@@ -0,0 +1,13 @@
+package com.harmonycare;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HarmonycareApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(HarmonycareApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/harmonycare/HarmonycareApplicationTests.java
@@ -0,0 +1,13 @@
+package com.harmonycare;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class HarmonycareApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
